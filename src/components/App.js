aecf7ceb51107ac6db01aac538ad25dc104ec8b1
Arano Orkatz <oarano@lekooa.com>
2018-02-08 08:26:45 +0100

    Connect firebase

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
+import * as firebase from 'firebase';
+import config from '../firebase';
 
 class App extends Component {
+    constructor() {
+        super()
+        firebase.initializeApp(config)
+    }
+
+    componentWillMount() {
+        // juste avant que le component soit monté
+        const tweetRef = firebase.database().ref('tweets')
+        tweetRef.on('value', snapshot => {
+            this.setState({
+                //state = les données qu'il y a dans le component
+                tweets: snapshot.val()
+            })
+        })
+    }
     render() {
         return (
             <h1>Hello World</h1>
---
+++ src/firebase.js
@@ -0,0 +1,5 @@
+export default {
+    apiKey: "AIzaSyD2bFyWrvfg0e1VFUugTsAZlHcmO9IxoiM",
+    authDomain: "twitter-like-2a9fb.firebaseapp.com",
+    databaseURL: "https://twitter-like-2a9fb.firebaseio.com",
+}
